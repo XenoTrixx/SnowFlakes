c6209c267fbec4ca3d796092fb02c0e01f133cc2
XenoTrixx <XenoTrixx@users.noreply.github.com>
2018-01-18 07:21:35 +0100

    Update snowflake.js

--- snowflake.js
+++ snowflake.js
@@ -1,10 +1,10 @@
-class SonwFlake {
+lass SonwFlake {
 
     constructor (x, y){
         this.position = createVector(x, y)
         this.speedFaktor = random(0.2, gravity);
         this.offSet = random(0, width);
-        this.xIncrement = 0.1;
+        this.xIncrement = 0.07;
     }
 
     update(){
